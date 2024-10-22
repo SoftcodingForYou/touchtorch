cc623e14e0929e8b3bd7d47901fc07a1132ad3ca
David Baum <github@davidb.anonaddy.com>
2024-10-21 23:31:59 -0300

    V1.0.1 with upgraded Gradle plugins

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 # Touch Torch
 
-[![Google Play Store Badge](artwork/Download_on_Google_Play_Store_en.png)](https://play.google.com/store/apps/dev?id=5179187296765509205)
+## App logo
+![App logo](artwork/app_logo.png)
 
+## Description
 This is a simple Flutter/Dart-based mobile app: You touch the screen and the light turns on. The light stays on until you lift you finger off the screen!
 
+## Why another flashlight app?
 As unbelievable as it might seem, this, to our knowledge, is the first app that enables your device's flashlight as long as your finger is in contact with the screen.
 
+## Code structure
 The whole app is written with less than 100 lines of Flutter/Dart code.
 
 .lib >
 1. main.dart: app entry point: initializes app and flashlight environment
 2. home.dart: The single screen of the app. Contains a Stack() widget that holds background images and text
+
+## Artwork
+![App artwork 1](artwork/Page1.png)
+![App artwork 2](artwork/Page2.png)
+![App artwork 3](artwork/Page3.png)
+
+## License
+This project is licensed under the BSD 3-Clause License. See the [LICENSE](LICENSE) file for details.
+
+TL;DR: Do whatever you want with the code!
--- lib/home.dart
+++ lib/home.dart
@@ -6,69 +6,65 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final torchController = TorchController();
 
     return Scaffold(
-      body: Center(
-        child: MaterialButton(
-          padding: EdgeInsets.zero,
-          onPressed: () {},
-          child: Container(
-            color: Colors.blueGrey.shade900,
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  top: 35,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Flashlight stays on as long\nas you touch the screen",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(color: Colors.blueGrey.shade500)
+        body: Center(
+            child: MaterialButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+                child: Container(
+                  color: Colors.blueGrey.shade900,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Stack(alignment: Alignment.center, children: [
+                    Positioned(
+                      top: 35,
+                      child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                                "Flashlight stays on as long\nas you touch the screen",
+                                textAlign: TextAlign.end,
+                                style:
+                                    TextStyle(color: Colors.blueGrey.shade500)),
+                          ]),
+                    ),
+                    Image.asset(
+                      "assets/images/morse_alphabet.png",
+                      cacheWidth:
+                          (MediaQuery.of(context).devicePixelRatio * 200)
+                              .round(),
+                      color: Colors.blueGrey.shade800,
+                    ),
+                    Positioned(
+                      right: 15,
+                      bottom: 15,
+                      child: Image.asset(
+                        "assets/images/click_icon.png",
+                        cacheWidth:
+                            (MediaQuery.of(context).devicePixelRatio * 35)
+                                .round(),
+                        width: 35,
+                        color: Colors.white,
                       ),
-                    ]
-                  ),
-                ),
-                Image.asset(
-                  "assets/images/morse_alphabet.png",
-                  cacheWidth: (MediaQuery.of(context).devicePixelRatio * 200).round(),
-                  color: Colors.blueGrey.shade800,
-                ),
-                Positioned(
-                  right: 15,
-                  bottom: 15,
-                  child: Image.asset(
-                    "assets/images/click_icon.png",
-                    cacheWidth: (MediaQuery.of(context).devicePixelRatio * 35).round(),
-                    width: 35,
-                    color: Colors.white,
-                  ),
-                ),
-                GestureDetector(
-                  onTapDown: (details) {
-                    if (!isTorchActive(torchController)) {
-                      torchController.toggle();
-                    }
-                  },
-                  onTapUp: (details) {
-                    torchController.toggle();
-                  },
-                  onTapCancel: () {
-                    torchController.toggle();
-                  },
-                ),
-              ]
-            ),
-          )
-        )
-      )
-    );
+                    ),
+                    GestureDetector(
+                      onTapDown: (details) {
+                        if (!isTorchActive(torchController)) {
+                          torchController.toggle();
+                        }
+                      },
+                      onTapUp: (details) {
+                        torchController.toggle();
+                      },
+                      onTapCancel: () {
+                        torchController.toggle();
+                      },
+                    ),
+                  ]),
+                ))));
   }
 
   bool isTorchActive(TorchController torchController) {
